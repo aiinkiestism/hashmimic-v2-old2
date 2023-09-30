145edc8527965e5b4527aca70bb6c1e7306e5604
aiinkiestism <g.k.k.r.1228kc@gmail.com>
2023-09-30 22:25:11 +0900

    feat: add prettier and eslint related modules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,15 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  extends: [
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/node",
+    "standard-with-typescript",
+		"prettier",
+  ],
+  "rules": {
+		"@typescript-eslint/semi": "off",
+		"@typescript-eslint/consistent-type-imports": "off",
+		"curly": "off",
+		"@typescript-eslint/ban-ts-ignore": "off"
+	},
 };
